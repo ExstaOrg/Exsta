4da09a42a8ef74e7be929b425a2e0670368246e2
Z3R-0 <timmeermans@live.nl>
2024-08-07 15:16:50 +0200

    Some initial work for setting up EF

---
+++ Exsta/Shared/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared;
+public class User {
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
