d5a3fe5fd4b82ed8fa13b2b84a643db2d36786ee
Z3R-0 <timmeermans@live.nl>
2024-08-16 09:47:36 +0200

    Pushing some debug stuff

--- Exsta-frontend/Program.cs
+++ Exsta-frontend/Program.cs
@@ -6,13 +6,10 @@ WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Optional: Load environment-specific configuration (e.g., appsettings.Development.json)
-builder.Configuration.AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", optional: true, reloadOnChange: true);
-
 // Load the subscription key from configuration
 string? subscriptionKey = builder.Configuration["ApiManagement:SubscriptionKey"];
 
-Console.WriteLine("sub key:" + subscriptionKey);
+Console.WriteLine("Environment: " + builder.HostEnvironment.Environment + " sub key: " + subscriptionKey);
 
 builder.Services.AddScoped(sp => {
     HttpClient client = new() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
