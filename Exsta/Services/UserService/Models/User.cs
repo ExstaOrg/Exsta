814d567eee8f0da5d4c355159aa30ff03899c359
Z3R-0 <timmeermans@live.nl>
2024-09-06 09:45:53 +0200

    Resolve warnings

--- Exsta/Services/UserService/Models/User.cs
+++ Exsta/Services/UserService/Models/User.cs
@@ -2,7 +2,7 @@ namespace UserService.Models;
 
 public class User {
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string PasswordHash { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
 }
