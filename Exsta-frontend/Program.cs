cda15173373580b6c4de3ff90c75d5a3d1911d42
Andono <jordyberg@gmail.com>
2024-09-22 14:35:54 +0200

    Feature/implement basic frontend (#16)
    * Added Blazorise to the project for General styling of our pages
    
    * Removed Default Bootstrap From Blazer Project to ensure the newest version for Blazorize will be used
    
    * Implemented Header
    
    * Sidebar initial implementation
    
    * Name fix
    
    * Home Page implementation Desktop View Front-end
    
    * Created new Page and link to it in the Navbar
    
    * Initial User Screen, not completely done yet but all the elements are there
    
    * Login Page Finished + Minor tweaks NavMenu
    
    * Created Register Page and added navigation to the Page
    
    * Made a start with Register Page
    
    * Register Page Front-End Implementation
    
    * First attempt mobile format support
    
    * Temporary Mobile Navigation Layout checkpoint
    
    * Mobile Friendly Navigation Bar
    
    * Code Cleanup
    
    ---------
    
    Co-authored-by: Z3R-0 <timmeermans@live.nl>

--- Exsta-frontend/Program.cs
+++ Exsta-frontend/Program.cs
@@ -1,10 +1,20 @@
 using Exsta_frontend;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddScoped(sp => {
     HttpClient client = new() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
