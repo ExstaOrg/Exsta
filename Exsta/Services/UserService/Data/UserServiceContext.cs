8a4e304fe47d63b9285db1512a94887c2060eb4a
Z3R-0 <timmeermans@live.nl>
2024-11-28 20:05:02 +0100

    Added database migration for new field

--- Exsta/Services/UserService/Data/UserServiceContext.cs
+++ Exsta/Services/UserService/Data/UserServiceContext.cs
@@ -5,4 +5,15 @@ using Shared.Data;
 namespace UserService.Data;
 public class UserServiceDbContext(DbContextOptions<UserServiceDbContext> options) : BaseDbContext(options) {
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<User>(entity => {
+            entity.HasKey(u => u.Id);
+            entity.Property(u => u.Roles)
+                  .HasConversion(
+                      v => string.Join(',', v),    // Serialize roles to a single string
+                      v => v.Split(',', StringSplitOptions.RemoveEmptyEntries)) // Deserialize back to array
+                  .IsRequired();
+        });
+    }
 }
---
+++ Exsta/Services/UserService/Migrations/20241126194943_AddRolesToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Roles",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Roles",
+                table: "Users");
+        }
+    }
+}
