53d0188bb4167894b9d7167d5601d318512c978e
Z3R-0 <timmeermans@live.nl>
2024-11-28 20:14:34 +0100

    Feature/124 implement jwt authentication (#19)
    * Renamed shared projects for compatibility with namespaces
    
    * Implement first draft of authentication service
    
    * Added database migration for new field
    
    * Added Swagger api key support
    
    * Added attributes to UserController so that JWT authentication is enforced properly

--- Exsta-Shared/Domain/User.cs
+++ Exsta-Shared/Domain/User.cs
@@ -5,4 +5,6 @@ public class User {
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string PasswordHash { get; set; }
+
+    public required string[] Roles { get; set; }
 }
--- Exsta/Services/UserService/Program.cs
+++ Exsta/Services/UserService/Program.cs
@@ -1,6 +1,12 @@
 using Azure.Identity;
+using Backend_Shared.Application;
+using Exsta_Shared.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Shared.Middleware;
+using System.Text;
 using UserService.Data;
 using UserService.Repositories;
 
@@ -26,11 +32,79 @@ builder.Services.AddDbContext<UserServiceDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UserServiceSqlServer")));
 
 // Add services to the container.
+builder.Services.AddTransient<AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    var securityScheme = new OpenApiSecurityScheme {
+        Name = "JWT Authentication",
+        Description = "Enter your JWT token in this field",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    };
+
+    c.AddSecurityDefinition("Bearer", securityScheme);
+
+    var securityRequirement = new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    };
+
+    c.AddSecurityRequirement(securityRequirement);
+
+    // Add API Key Authentication scheme
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme {
+        Name = "x-api-key", // Header name
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Description = "Enter your API key"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services
+    .AddAuthentication(x => {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x => {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["auth-service-private-key"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -40,10 +114,14 @@ if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName.Equals("L
     app.UseSwaggerUI();
 }
 
+app.MapPost("/authenticate", (User user, AuthService authService)
+    => authService.GenerateToken(user));
+
 app.UseHttpsRedirection();
 
 app.UseCors("AllowSpecificOrigin");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseMiddleware<ApiKeyMiddleware>();
