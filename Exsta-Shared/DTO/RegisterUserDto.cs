e98bd3f9c764c1a5bd3e57430f507b906b0dd848
Z3R-0 <timmeermans@live.nl>
2024-12-17 22:34:56 +0100

    Added DTO and trying to add interface

---
+++ Exsta-Shared/DTO/RegisterUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Exsta_Shared.DTO;
+
+public class RegisterUserDto {
+    public required string Email { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public string[]? Roles { get; set; }
+}
---
+++ Exsta/Services/UserService/Application/IRegisterUserApplicationService.cs
@@ -0,0 +1,5 @@
+﻿namespace UserService.Application;
+
+public interface IRegisterUserApplicationService {
+    public Task<(bool Success, string[]? Errors)> RegisterUserAsync(RegisterUserDto registerUserDto);
+}
